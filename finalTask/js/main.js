f86b614a4efe3a2e217bbe613abb54956913f7f5
v.smilyanskiy <v.smilyanskiy@betinvest.com>
2019-12-27 16:23:07 +0200

    last task in progress

--- finalTask/js/main.js
+++ finalTask/js/main.js
@@ -26,6 +26,7 @@ function createList(data) {
     mainList.body = [];
     mainList.previous = data.previous;
     mainList.next = data.next;
+    mainList.count = data.count;
     for(let arr in data.results) {
         let row = {};
         for(let key in data.results[arr]) {
@@ -53,27 +54,31 @@ function loadList(list) {
         <td>${list.body[i].name}</td>
         <td>${list.body[i].birth_year}</td>
         <td>${list.body[i].gender}</td>
-        <td><button onclick="getName(\'${list.body[i].homeworld}\')">Show</button></td>
-        <td><button onclick="getName(${list.body[i].species[0]})">Show</button></td>
+        <td><button onclick="getName(\'${list.body[i].homeworld}\', \'${list.body[i].name}\')">Show</button></td>
+        <td><button onclick="getName(\'${list.body[i].species[0]}\')">Show</button></td>
         </tr>`;
     }
     document.querySelector('table tbody').insertAdjacentHTML('afterbegin', body);
     //Переключение страниц
     let buttons = document.querySelector('.scroll-buttons');
-    buttons.innerHTML = '';
-    buttons.insertAdjacentHTML("afterbegin", "<div>prev</div><div>next</div>");
-    buttons.children[0].addEventListener('click', function() {
-        if(list.previous !== null) {
-            url = list.previous;
-            getList();
-        }
-    });
-    buttons.children[1].addEventListener('click', function() {
-        if(list.next !== null) {
-            url = list.next;
-            getList();
-        }
-    });
+    if(list.count >= 10) {
+        buttons.innerHTML = '';
+        buttons.insertAdjacentHTML("afterbegin", "<div></div><div></div>");
+        buttons.children[0].addEventListener('click', function() {
+            if(list.previous !== null) {
+                url = list.previous;
+                getList();
+            }
+        });
+        buttons.children[1].addEventListener('click', function() {
+            if(list.next !== null) {
+                url = list.next;
+                getList();
+            }
+        });
+    } else {
+        document.querySelector('.scroll-buttons').innerHTML = '';
+    }
     //устанавливаем обрабодчик на поиск после загрузки дом
     let inputSearch = document.querySelector('.search');
     inputSearch.addEventListener("keyup", search);
@@ -96,14 +101,50 @@ function showFakeWindow() {
 }
 
 
-function getName(link) {
+function getName(link, characterName) {
     fetch(`${link}?format=json`).then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data.name);
-        return data.name;
+        //console.log(data);
+        return getAdditionalInfo(data, characterName);
     }).catch(function(error) {
         console.error(error);
         return '';
     });
+}
+
+function getAdditionalInfo(data, characterName) {
+    let info = document.querySelector('.additional-info');
+    info.innerHTML = '';
+    let body = `
+    <span>close</span>
+    <h4>${characterName} Homeworld</h4>
+    <ul>
+        <li>Name: <span>${data.name}</span></li>
+        <li>Climate: <span>${data.climate}</span></li>
+        <li>Terrain: <span>${data.terrain}</span></li>
+        <li>Population: <span>${data.population}</span></li>
+    </ul>`;
+    info.innerHTML = body;
+    info.addEventListener('mousedown', function(e) {
+        document.body.appendChild(info);
+        info.style.left = e.pageX - info.offsetWidth / 2 + 'px';
+        info.style.top = e.pageY - info.offsetHeight / 2 + 'px';
+        info.addEventListener('mousemove', function(e) {
+            info.style.left = e.pageX - info.offsetWidth / 2 + 'px';
+            info.style.top = e.pageY - info.offsetHeight / 2 + 'px';
+        });
+        info.addEventListener('dragstart', function() {
+            return false;
+        });
+    });
+    info.addEventListener('mouseup', function() {
+        info.onmousemove = null;
+        document.onmousemove = null;
+        info.onmousedown = null;
+        document.onmousedown = null;
+    });
+    
+    
+    
 }
